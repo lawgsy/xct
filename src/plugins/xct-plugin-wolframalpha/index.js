8798ddc61edd06ec27fc7166b587b332bcaa12af
lawgsy <lawgsy@gmail.com>
2017-11-21 18:39:58 +0100

    Add notify

--- src/plugins/xct-plugin-wolframalpha/index.js
+++ src/plugins/xct-plugin-wolframalpha/index.js
@@ -2,6 +2,7 @@ module.exports =
   ({vueObj, common}, s) => {
     var {cmd, input} = common.parseInput(s);
     if(input.length>0) {
+      vueObj.notify('Searching wolfram...')
       var query        = common.webUtils.urlEncode(input);
       vueObj.output =
         common.webUtils.webView(`http://m.wolframalpha.com/input/?i=${query}`);
